61595b0ae1a5c6668688f65fb46a08a606bdc3f8
Kartik Chinda <kartik.chinda@gmail.com>
2024-11-05 13:57:19 +0530

    Feature: Added the Hero section.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,30 @@
+import Image from "next/image";
 import React from "react";
+import BlogHero from "@/public/blogHero.jpg";
 
 const Hero = () => {
   return (
-    <div className="h-[80vh] flex flex-col lg:flex-row justify-between items-center">
-      <div className="w-full lg:w-[70%]">
-        <div>The Mock Blog</div>
-        <div>It's always an empty page, until it is not.</div>
+    <div className="min-h-[80vh] lg:flex lg:justify-around lg:items-center p-8">
+      <div className="w-full flex justify-center items-center lg:justify-start lg:items-start flex-col">
+        <div className=" font-heading-playfair  font-extrabold text-[45px] lg:text-8xl mb-8">
+          The Mock Blog
+        </div>
+        <div className=" font-subtext-heebo font-light mb-10 lg:mb-0 hero-subtext">
+          It's always an empty page, until it is not.
+          <p className=" hero-subtext">
+            So dive in, explore, and if you're ready, leave your mark. And who
+            knows? Your next big idea might just start right here.
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center  lg:w-[50%]">
+        <Image
+          src={BlogHero}
+          alt="Notebook"
+          width={300}
+          height={300}
+          className=" rounded-2xl border-8 border-[#dbb0a15b]"
+        />
       </div>
     </div>
   );
