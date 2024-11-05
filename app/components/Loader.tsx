4c6e6c3b46a74421ee1fcef8769c4f29ec6783fd
Kartik Chinda <kartik.chinda@gmail.com>
2024-11-05 21:20:59 +0530

    Feat: Added a circleSpinner.

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,9 +1,9 @@
-import { FadeLoader } from "react-spinners";
+import { CircleLoader } from "react-spinners";
 
 const Loader = ({ loading }: { loading: boolean }) => {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center">
-      <FadeLoader color="black" loading={loading} />
+      <CircleLoader color="black" loading={loading} />
     </div>
   );
 };
